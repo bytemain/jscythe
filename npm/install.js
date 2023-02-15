5bd6ea91f4b733945e776df58980d46871bb1185
野声 <lijiacheng.ljc@antgroup.com>
2023-02-16 02:48:05 +0800

    chore: update binary installer

--- npm/install.js
+++ npm/install.js
@@ -1,7 +1,3 @@
 const { binary } = require("./binary");
-if (binary.exists()) {
-  console.log(`\n${binary.name} is already installed, reinstalling...`);
-  binary.uninstall();
-}
 
 binary.install();
